5cf9bd603d1ecc45b605d981f8be45d77798bd11
Nikolay Salov <elluminatte@gmail.com>
2014-12-28 00:26:35 +0300

    added mathcore class and added it to autoload

---
+++ app/classes/Elluminate/Math/LogisticRegression.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elluminate
+ * Date: 28.12.14
+ * Time: 1:07
+ */
+
+namespace Elluminate\Math;
+
+
+class LogisticRegression {
+    public static function test() {
+        return 'im working!';
+    }
+
+}
