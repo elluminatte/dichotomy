32fca90c47ecb7051dfcca1a4f9599c9dfe5e54f
Nikolay Salov <elluminatte@gmail.com>
2015-01-19 13:41:59 +0300

    локализовал сообщения системы

---
+++ app/lang/ru/reminders.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Пароли должны совпадать и состоять не менее, чем из 6 символов.",
+
+	"user" => "Пользователь с таким E-mail не найден.",
+
+	"token" => "Этот атрибут для сброса пароля является недействительным.",
+
+	"sent" => "Письмо для напоминания пароля было успешно отправлено!",
+
+	"reset" => "Пароль был успешно сброшен!",
+
+);
