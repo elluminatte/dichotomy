93335985782c37d142102bd527393d4b18fdc980
Nikolay Salov <elluminatte@gmail.com>
2015-01-20 14:40:30 +0300

    Поправил родительский метод

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,9 +11,7 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		 $this->call('AdminSeeder');
-
-		$this->command->info('Admin info seeded!');
+		 $this->call('UserTableSeeder');
 	}
 
 }
