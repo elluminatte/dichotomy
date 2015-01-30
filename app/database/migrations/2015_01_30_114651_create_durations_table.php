058f8cb0c8e01434fc9812e20a5c138931a4640e
Nikolay Salov <elluminatte@gmail.com>
2015-01-30 12:40:29 +0300

    миграция для таблицы Время корректности решения

---
+++ app/database/migrations/2015_01_30_114651_create_durations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDurationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// создаем таблицу с возможными вариантами корректности решения
+		Schema::create('durations', function( $table ) {
+			$table->increments('id')->unsigned();
+			$table->string('name', 50);
+			//будем хранить в часах, а потом добавлять с помощью Carbon
+			$table->smallInteger('duration');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//удаляем таблицу при откате миграции
+		Schema::drop('durations');
+		//
+	}
+
+}
