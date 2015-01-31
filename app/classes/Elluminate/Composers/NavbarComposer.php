46ff777282f54b0b725e77c183c220e7c8e8fe26
Nikolay Salov <elluminatte@gmail.com>
2015-01-31 14:56:48 +0300

    добавил реализацию составителя шаблнов, зарегистрировал его для прорисовки верхнего меню

---
+++ app/classes/Elluminate/Composers/NavbarComposer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elluminate
+ * Date: 31.01.15
+ * Time: 14:38
+ */
+namespace Elluminate\Composers;
+
+/** составитель шаблона для верхнего меню
+ * Class NavbarComposer
+ * @package Elluminate\Composers
+ */
+class NavbarComposer {
+
+    /** функция будет вызываться каждый раз, когда будет отрисовываться нужный шаблон
+     * регистрация составителя находится в app/composers.php
+     * @param $view
+     */
+    public function compose($view) {
+
+    }
+}
