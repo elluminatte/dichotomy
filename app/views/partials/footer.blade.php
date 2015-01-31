e7466121afa87ef99eddcc31d6bbfb7cd837e999
Nikolay Salov <elluminatte@gmail.com>
2015-01-31 21:47:34 +0300

    прикрепил подвал к низу страницы

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,7 +1,9 @@
-<div class="container">
-    <div class="row">
-        <div class="well">
-            Diagnostic - Оценка вероятности событий
+<div class="footer navbar-fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="well">
+                Типа подвал, не знаю - надо или нет.
+            </div>
         </div>
     </div>
 </div>
