3ff44c9bfd08e51840e00abc71987dcb411565fe
Nikolay Salov <elluminatte@gmail.com>
2015-02-09 15:31:01 +0300

    коэффициенты ищутся

--- app/classes/Elluminate/Math/LogisticRegression.php
+++ app/classes/Elluminate/Math/LogisticRegression.php
@@ -9,100 +9,220 @@
 namespace Elluminate\Math;
 
 
-/**
+/** аппарат логистической регрессии
  * Class LogisticRegression
  * @package Elluminate\Math
  */
 class LogisticRegression {
 
     /**
-     * @var - массив с обучающим примером
+     * максимальное кол-во регрессоров
      */
-    private $trainData;
+    const  MAX_COVARIATES_NUM = 12;
 
     /**
-     * @var - массив регрессоров
+     * максимальное кол-во экспериментов
      */
-    private $covariates;
+    const MAX_EXPERIMENTS_NUM = 10000;
 
     /**
-     * @var - массив с наименованиями переменных, функции и регрессоров
+     * минимальное кол-во регрессоров
      */
-    private $names;
+    const MIN_COVARIATES_NUM = 1;
 
     /**
-     * @var - массив (вектор) значений функции
+     * степень покрытия регрессоров экспериментами
      */
-    private $regression;
+    const EXPERIMENTS_COVERAGE = 10;
 
     /**
-     * @var - массив (вектор) коэффициентов логистической регрессии
+     * максимальное кол-во раз ухудшения найденных коэффициентов, после которого можно считать, что метод расходится
      */
-    private $coefficients;
+    const MAX_WORSE_TIMES = 4;
 
     /**
-     * @var int - максимальное кол-во итераций при поиске коэффициентов методом Ньютона-Рафсона
+     * максималное кол-во итераций метода Ньютона-Рафсона
      */
-    private $maxIterations;
+    const MAX_NEWTON_RAPHSON_ITERATIONS = 1000;
+
+    const EPSILON = 0.001;
+
+    const JUMP_VALUE = 1000;
 
     /**
-     * @var float - точность поиска коэффициентов
+     * @var MathCore - математическое ядро
      */
-    private $epsilon;
+    protected $oMath;
 
     /**
-     * @var int - размер прыжка метода Ньютона-Рафсона, при котором можно считать, что метод не сошелся
+     * @var - кол-во регрессоров
      */
-    private $jumpFactor;
+    private $iCovariatesNum;
 
     /**
-     * @var - массив с логом процесса работы класса
-     * ToDo: может сделать коды ситуаций, и если ситуации наступают, то обучение прошло неправильно
+     * @var - кол-во экспериментов
      */
-    public $log;
+    private $iExperimentsNum;
 
-    public function __construct($trainData, $maxIterations = 1000, $epsilon = 0.001, $jumpFactor = 1000) {
-        $log = array();
-        $this->trainData = $trainData;
-        $this->maxIterations = $maxIterations;
-        $this->epsilon = $epsilon;
-        $this->jumpFactor = $jumpFactor;
-        unlink($trainData);
+    public $aCovariates;
+
+    public $aRegression;
+
+    public $aCoefficients;
+
+
+    /**
+     * @return mixed
+     */
+    public function getCoefficients()
+    {
+        return $this->aCoefficients;
     }
 
-    public function estimateCoefficients() {
-        $this->separateCovRegNames();
-        $this->coefficients = $this->computeBettaVector();
+    public function __construct(MathCore $oMath) {
+        // внедрли зависимость - мат. ядро
+        $this->oMath = $oMath;
     }
 
-    /**
-     * анализирует данные, полученные из файла обучающнй выборки
-     * и выделяет из них массив регрессоров, вектор значений целевой функции
-     * и вектор наименований регрессоров и функции
+    public function setTrainingSet($aTrainingSet)
+    {
+        $bCheckResult = $this->checkTrainingSet($aTrainingSet);
+        if($bCheckResult) {
+            $this->separateRegressionAndCovariates($aTrainingSet);
+        }
+        unset($aTrainingSet);
+    }
+
+    /** разделяет регрессоры и значения функции
+     * @param $aTrainingSet - массив обучающей выборки
+     * @return bool - результат разделения
      */
-    private function separateCovRegNames() {
-        $this->covariates = $this->separateCovariates();
-        $this->regression = $this->separateRegression();
-        $this->names = $this->separateNames();
+    private function separateRegressionAndCovariates($aTrainingSet) {
+        $aRegression = [];
+        $aCovariates = [];
+            foreach($aTrainingSet as $key => $value) {
+                // регрессия - первый столбец
+                $aRegression[] = $value[0];
+                $value[0] = 1;
+                // регрессоры - все остальное
+                $aCovariates[] = $value;
+            }
+        unset($aTrainingSet);
+        $this->aCovariates = $aCovariates;
+        unset($aCovariates);
+        $this->aRegression = $aRegression;
+        unset($aRegression);
+        return true;
     }
 
-    private function computeBettaVector() {
-
+    public function trainModel() {
+        $aCoefficients = $this->computeCoefficients();
+        if($aCoefficients && count($aCoefficients))
+            $this->aCoefficients = $aCoefficients;
     }
 
-    private function separateCovariates() {
-        $covariates = array();
-        return $covariates;
+    public function estimate() {
+        if(isset($this->aCoefficients) && count($this->aCoefficients))
+            return $this->constructActualFuncVector($this->aCovariates, $this->aCoefficients);
+        return [];
     }
 
-    private function separateRegression() {
-        $regression = array();
-        return $regression;
+    public function computeCoefficients() {
+        $aCoeffVector = $this->oMath->constructVector($this->iCovariatesNum + 1);
+        $aFinalCoeffVector = $aCoeffVector;
+        $aActualRegVector = $this->constructActualFuncVector($this->aCovariates, $aCoeffVector);
+        $fMse = $this->oMath->meanSqError($aActualRegVector, $this->aRegression);
+        $iWorseTimes = 0;
+        for( $i = 0; $i < self::MAX_NEWTON_RAPHSON_ITERATIONS; ++$i) {
+            $aNextCoeffVector = $this->constructNextCoeffVector($aCoeffVector, $this->aCovariates, $this->aRegression, $aActualRegVector);
+            if(!$aNextCoeffVector) throw new \Elluminate\Exceptions\SingularException;
+            if(!$this->checkChanging($aCoeffVector, $aNextCoeffVector)) return $aFinalCoeffVector;
+            if($this->checkJumping($aCoeffVector, $aNextCoeffVector)) return $aFinalCoeffVector;
+            $aActualRegVector = $this->constructActualFuncVector($this->aCovariates, $aNextCoeffVector);
+
+            $fNextMse = $this->oMath->meanSqError($aActualRegVector, $this->aRegression);
+            if($fNextMse > $fMse) {
+                ++$iWorseTimes;
+                if($iWorseTimes > self::MAX_WORSE_TIMES)
+                    return $aFinalCoeffVector;
+//                $aCoeffVector = $aNextCoeffVector;
+                for($k = 0; $k < count($aCoeffVector); ++$k)
+                    $aCoeffVector[$k] = ( $aCoeffVector[$k] + $aNextCoeffVector[$k] ) / 2;
+            }
+            else {
+                $aCoeffVector = $aNextCoeffVector;
+                $aFinalCoeffVector = $aCoeffVector;
+                $iWorseTimes = 0;
+            }
+            $fMse = $fNextMse;
+        }
+        return $aFinalCoeffVector;
     }
 
-    private function separateNames() {
-        $names = array();
-        return $names;
+    private function checkChanging($aOldCoeffVector, $aNewCoeffVector) {
+        $iRows = count($aOldCoeffVector);
+        for($i = 0; $i < $iRows; ++$i)
+            if( abs($aOldCoeffVector[$i] - $aNewCoeffVector[$i]) > self::EPSILON )
+                return true;
+        return false;
+    }
+
+    private function checkJumping($aOldCoeffVector, $aNewCoeffVector) {
+        $iRows = count($aOldCoeffVector);
+        for($i = 0; $i < $iRows; ++$i) {
+            if( $aOldCoeffVector[$i] === 0 ) return false;
+            if( ( abs($aOldCoeffVector[$i] - $aNewCoeffVector[$i]) / abs($aOldCoeffVector[$i]) ) > self::JUMP_VALUE )
+                return true;
+        }
+        return false;
+    }
+
+    private function constructNextCoeffVector($aCoeffVector, $aCovMatrix, $aRegVector, $aActualRegVector) {
+        $aTranspCovMatrix = $this->oMath->matrixTranspose($aCovMatrix);
+        $a = $this->oMath->computeXtilde($aActualRegVector, $aCovMatrix);
+        $b = $this->oMath->matrixProduct($aTranspCovMatrix, $a);
+        $c = $this->oMath->matrixInverse($b);
+        if(!$c) return false;
+        $d = $this->oMath->matrixProduct($c, $aTranspCovMatrix);
+        $aSubtract = $this->oMath->vectorSubtraction($aRegVector, $aActualRegVector);
+        $e = $this->oMath->matrixVectorProduct($d, $aSubtract);
+        $aResult = $this->oMath->vectorAddition($aCoeffVector, $e);
+        return $aResult;
+    }
+
+    private function constructActualFuncVector($aCovMatrix, $aCoeffVector) {
+        $aResult = $this->oMath->constructVector($this->iExperimentsNum);
+        for($i = 0; $i < $this->iExperimentsNum; ++$i) {
+            $z = 0.0;
+            for($j = 0; $j< $this->iCovariatesNum + 1; ++$j)
+                $z += $aCovMatrix[$i][$j] * $aCoeffVector[$j];
+            $p = 1 / (1 + exp(-$z));
+            $aResult[$i] = $p;
+        }
+        return $aResult;
+    }
+
+    /** проверяет обучающую выборку на соответствие требованиям
+     * @param $aTrainingSet - массив обучающей выборки
+     * @return bool - результат проверки, истина - все нормально, ложь - что-то не так
+     * @throws \Elluminate\Exceptions\TrainSetFileException - исключение
+     */
+    private function checkTrainingSet($aTrainingSet) {
+        foreach($aTrainingSet as $row)
+            foreach($row as $value)
+                //если не заполнено или не является числом
+                if(!isset($value) || !is_numeric($value))
+                    throw new \Elluminate\Exceptions\TrainSetFileException("Проверьте, что все поля обучающей выборки заполнены и являются числами.");
+        // один столбец - значение функции, поэтому регрессоров на 1 меньше
+        $this->iCovariatesNum = count($aTrainingSet[0]) - 1;
+        $this->iExperimentsNum = count($aTrainingSet);
+        // проверяем кол-во регрессоров
+        if( $this->iCovariatesNum > self::MAX_COVARIATES_NUM || $this->iCovariatesNum < 1 )
+            throw new \Elluminate\Exceptions\TrainSetFileException("Количество параметров регрессии не должно быть больше ".self::MAX_COVARIATES_NUM);
+        // проверяем кол-во экспериментов
+        if( $this->iExperimentsNum < $this->iCovariatesNum*self::EXPERIMENTS_COVERAGE || $this->iExperimentsNum > self::MAX_EXPERIMENTS_NUM )
+            throw new \Elluminate\Exceptions\TrainSetFileException("Количество опытов должно быть не меньше ".$this->iCovariatesNum*self::EXPERIMENTS_COVERAGE." и не больше ".self::MAX_EXPERIMENTS_NUM);
+        return true;
     }
 
 
