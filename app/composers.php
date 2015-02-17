6013504cd7511da8b84251562d5d20a801d3f7f9
Nikolay Salov <elluminatte@gmail.com>
2015-02-17 14:13:20 +0300

    пока составители видов оказались не нужны

--- app/composers.php
+++ app/composers.php
@@ -6,4 +6,4 @@
  * Time: 14:45
  */
 // регистрация составителей шаблонов
-View::composer('partials.navbar', '\Elluminate\Composers\NavbarComposer');
+//View::composer('partials.navbar', '\Elluminate\Composers\NavbarComposer');
