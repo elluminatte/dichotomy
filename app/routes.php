919d78bc377d52db5fb707464ecd53fe54a2a92e
Nikolay Salov <elluminatte@gmail.com>
2015-02-17 14:19:50 +0300

    новые маршруты, вынесено создание меню, иначе не работают именованные маршруты, на github есть такое issue

--- app/routes.php
+++ app/routes.php
@@ -32,6 +32,11 @@ Route::group(array('prefix' => 'admin'), function()
 	Route::post('models/store', array('before' => 'csrf', 'as' => 'models.store', 'uses' => 'ModelController@store'));
 	Route::get('models/destroy/{iModelId}', array('as' => 'models.destroy', 'uses' => 'ModelController@destroy'));
 });
+
+Route::group(array('prefix' => 'client'), function() {
+
+});
+
 // Confide routes
 Route::get('users/create', 'UsersController@create');
 Route::post('users', 'UsersController@store');
@@ -43,3 +48,35 @@ Route::post('users/forgot_password', 'UsersController@doForgotPassword');
 Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
 Route::post('users/reset_password', 'UsersController@doResetPassword');
 Route::get('users/logout', 'UsersController@logout');
+
+// пришлось вынести меню сюда, иначе не работают именованные маршруты
+// меню авторизации
+Menu::make('authNavBar', function($menu){
+	// если пользователь залогинился, то покажем кто он и кнопку на выхож
+	if(\Auth::user()) {
+		$menu->add('<i class="fa fa-user"></i>  Вы вошли как '.\Auth::user()->email);
+		$menu->add('<i class="fa fa-sign-out"></i> Выйти', 'users/logout');
+	}
+	// иначе предложим войти или зарегистрироваться
+	else {
+		$menu->add('<i class="fa fa-sign-in"></i> Войти', 'users/login');
+		$menu->add('<i class="fa fa-user-plus"></i> Зарегистрироваться', 'users/create');
+	}
+});
+
+// админское меню
+Menu::make('adminNavBar', function($menu) {
+	// проверим, есть ли у пользователя роль администратора
+	if(\Entrust::hasRole('administrator')) {
+		$menu->add('<i class="fa fa-cogs"></i> Управление')
+			->add('<i class="fa fa-folder-open"></i> Каталог проблемных ситуаций</a>', array('route'  => 'situations.list'));
+	}
+});
+
+// меню зарегистрированного пользователя
+Menu::make('userNavBar', function($menu) {
+	// проверим, есть ли у пользователя роли юзера
+	if(\Entrust::hasRole('user')) {
+
+	}
+});
