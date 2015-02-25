de89a71c964a85ff5d68201a549d85d018f013b5
Nikolay Salov <elluminatte@gmail.com>
2015-02-25 19:17:38 +0300

    виды для уведомлений о необходимости обратной связи

---
+++ app/views/client/notifications/expired_evaluations.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-dismissible alert-danger">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <p><i class="fa fa-hand-o-right"></i> У Вас имеются <a class="alert-link" href="{{ URL::route('evaluations.list') }}">задачи, срок подтверждения которых истек.</a></p>
+    <p><i class="fa fa-exclamation-triangle"></i> Возможность решения задач классификации временно заблокирована для Вас до момента, пока Вы не осуществите обратную связь</p>
+</div>
---
+++ app/views/client/notifications/waiting_evaluations.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-dismissible alert-warning">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <p><i class="fa fa-hand-o-right"></i> У Вас имеются <a class="alert-link" href="{{ URL::route('evaluations.list') }}">задачи, которые ожидают Вашей обратной связи</a></p>
+</div>
